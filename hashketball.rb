50e5032a4843d04b4cedfd880065a83d872d4040
adventurous-format-6947 <bminczu@gmail.com>
2020-10-14 16:35:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
     team_name: "Brooklyn Nets",
     colors: ["Black", "White"],
     players:[
-      alan_anderson: {
+       {
         player_name:"Alan Anderson",
         number: 0,
         shoe: 6,
@@ -16,7 +16,7 @@ def game_hash
         blocks: 1,
         slam_dunks: 1},
 
-      reggie_evans: {
+      {
         player_name:"Reggie Evans",
         number: 30,
         shoe: 14,
@@ -27,7 +27,7 @@ def game_hash
         blocks: 12,
         slam_dunks: 7},
 
-      brook_lopez: {
+       {
         player_name:"Brook Lopez",
         number: 11,
         shoe: 17,
@@ -38,7 +38,7 @@ def game_hash
         blocks: 1,
         slam_dunks: 15},
 
-      mason_plumlee: {
+       {
         player_name:"Mason Plumlee",
         number: 1,
         shoe: 19,
@@ -49,7 +49,7 @@ def game_hash
         blocks: 8,
         slam_dunks: 5},
 
-      jason_terry: {
+       {
         player_name:"Jason Terry",
         number: 15,
         shoe: 15,
@@ -67,7 +67,7 @@ def game_hash
     colors: ["Turquoise", "Purple"],
 
     players: [
-    jeff_adrien: {
+    {
         player_name:"Jeff Adrien",
         number: 4,
         shoe: 18,
@@ -78,7 +78,7 @@ def game_hash
         blocks: 7,
         slam_dunks: 2 },
 
-      bismack_biyombo: {
+      {
         player_name:"Bismack Biyombo",
         number: 0,
         shoe: 16,
@@ -89,7 +89,7 @@ def game_hash
         blocks: 15,
         slam_dunks: 10 },
 
-      desagna_diop: {
+       {
           player_name:"DeSagna Diop",
           number: 2,
           shoe: 14,
@@ -100,7 +100,7 @@ def game_hash
           blocks: 5,
           slam_dunks: 5 },
 
-      ben_gordon: {
+       {
           player_name:"Ben Gordon",
           number: 8,
           shoe: 15,
@@ -111,7 +111,7 @@ def game_hash
           blocks: 1,
           slam_dunks: 0 },
 
-      kemba_walker: {
+       {
           player_name:"Kemba Walker",
           number: 33,
           shoe: 15,
