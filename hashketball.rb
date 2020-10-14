5d2fdd044ed1700883d3892076d702ba5cd58bc0
adventurous-format-6947 <bminczu@gmail.com>
2020-10-14 16:31:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
     team_name: "Brooklyn Nets",
     colors: ["Black", "White"],
     players:[
-      alan_anderson: {
+      {
         player_name:"Alan Anderson",
         number: 0,
         shoe: 6,
@@ -16,7 +16,7 @@ def game_hash
         blocks: 1,
         slam_dunks: 1},
 
-      reggie_evans: {
+      {
         player_name:"Reggie Evans",
         number: 30,
         shoe: 14,
@@ -27,7 +27,7 @@ def game_hash
         blocks: 12,
         slam_dunks: 7},
 
-      brook_lopez: {
+      {
         player_name:"Brook Lopez",
         number: 11,
         shoe: 17,
@@ -38,7 +38,7 @@ def game_hash
         blocks: 1,
         slam_dunks: 15},
 
-      mason_plumlee: {
+       {
         player_name:"Mason Plumlee",
         number: 1,
         shoe: 19,
@@ -49,7 +49,7 @@ def game_hash
         blocks: 8,
         slam_dunks: 5},
 
-      jason_terry: {
+      {
         player_name:"Jason Terry",
         number: 15,
         shoe: 15,
