1125225f333e2f44990dd072b48e508e238465eb
adventurous-format-6947 <bminczu@gmail.com>
2020-10-14 16:35:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,7 +8,7 @@ def game_hash
        {
         player_name:"Alan Anderson",
         number: 0,
-        shoe: 6,
+        shoe: 16,
         points: 22,
         rebounds: 12,
         assists: 12,
