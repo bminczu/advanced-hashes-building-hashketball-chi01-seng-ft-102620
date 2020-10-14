8f5fc3a2deaf1d9f539828eb3e72e32946a7343b
adventurous-format-6947 <bminczu@gmail.com>
2020-10-14 15:38:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@ hashketball = {
   home: {
     team_name: "Brooklyn Nets",
     colors: ["Black", "White"],
-    players:
+    players:[
       alan_anderson: {
         player_name:"Alan Anderson",
         number: "0",
@@ -13,6 +13,7 @@ hashketball = {
         steals: "3",
         blocks: "1",
         slam_dunks: "1"},
+        
       reggie_evans: {
         player_name:"Reggie Evans",
         number: "30",
@@ -23,6 +24,7 @@ hashketball = {
         steals: "12",
         blocks: "12",
         slam_dunks: "7"},
+        
       brook_lopez: {
         player_name:"Brook Lopez",
         number: "11",
@@ -33,10 +35,31 @@ hashketball = {
         steals: "3",
         blocks: "1",
         slam_dunks: "15"},
+        
+      mason_plumlee: {
+        player_name:"Mason Plumlee",
+        number: "1",
+        shoe:"19",
+        points:"26",
+        rebounds:"11",
+        assists: "6",
+        steals: "3",
+        blocks: "8",
+        slam_dunks: "5"},
 
-      }
-      }]
+      jason_terry: {
+        player_name:"Jason Terry",
+        number: "15",
+        shoe:"15",
+        points:"19",
+        rebounds:"2",
+        assists: "2",
+        steals: "4",
+        blocks: "11",
+        slam_dunks: "1"},
+      ]
   }
+  
   away:{
     team_name:
     colors:
